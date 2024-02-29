6a212e57ab7b9112d7709692532754394568a359
Changmin Han <hcmhcs0@ajou.ac.kr>
2024-02-29 18:18:49 +0900

    chore: 초기 프로젝트와 깃 연결

---
+++ src/main/java/tour/nonghaeng/NonghaengApplication.java
@@ -0,0 +1,13 @@
+package tour.nonghaeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonghaengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonghaengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tour/nonghaeng/NonghaengApplicationTests.java
@@ -0,0 +1,13 @@
+package tour.nonghaeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonghaengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
