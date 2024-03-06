4f87cacb9790bf583a41f86323b3cf6bbac2ed47
Changmin Han <hcmhcs0@ajou.ac.kr>
2024-03-06 12:50:14 +0900

    feat/exception: Exception 추가 및 ExceptionHandler 작성

--- src/main/java/tour/nonghaeng/global/exception/CustomOneException.java
+++ src/main/java/tour/nonghaeng/global/exception/CustomOneException.java
@@ -1,2 +1,13 @@
-package tour.nonghaeng.global.exception;public class CustomOneException {
+package tour.nonghaeng.global.exception;
+
+import tour.nonghaeng.global.exception.code.BaseErrorCode;
+import tour.nonghaeng.global.exception.code.GlobalErrorCode;
+
+public class CustomOneException extends NongHaengException{
+
+    public static final CustomOneException EXCEPTION = new CustomOneException();
+
+    public CustomOneException() {
+        super(GlobalErrorCode.ALL_NOT_FOUND);
+    }
 }
--- src/main/java/tour/nonghaeng/global/exception/NongHaengException.java
+++ src/main/java/tour/nonghaeng/global/exception/NongHaengException.java
@@ -1,2 +1,17 @@
-package tour.nonghaeng.global.exception;public class NongHaengException {
+package tour.nonghaeng.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import tour.nonghaeng.global.exception.code.BaseErrorCode;
+import tour.nonghaeng.global.exception.dto.ErrorReason;
+
+@AllArgsConstructor
+@Getter
+public class NongHaengException extends RuntimeException{
+
+    private BaseErrorCode baseErrorCode;
+
+    public ErrorReason getErrorReason() {
+        return this.baseErrorCode.getErrorReason();
+    }
 }
