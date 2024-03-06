5189cc1cc40322a01d13ad10b44382766c71a3e9
Changmin Han <hcmhcs0@ajou.ac.kr>
2024-03-06 11:12:23 +0900

    feat/converter: bankCode Converter 추가 및 적용

--- src/main/java/tour/nonghaeng/global/config/ConverterConfig.java
+++ src/main/java/tour/nonghaeng/global/config/ConverterConfig.java
@@ -1,7 +1,22 @@
 package tour.nonghaeng.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import tour.nonghaeng.global.converter.BankCodeConverter;
 
 @Configuration
-public class ConverterConfig {
+public class ConverterConfig implements WebMvcConfigurer {
+
+    private final BankCodeConverter bankCodeConverter;
+
+    public ConverterConfig(BankCodeConverter bankCodeConverter) {
+        this.bankCodeConverter = bankCodeConverter;
+    }
+
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(bankCodeConverter);
+    }
 }
