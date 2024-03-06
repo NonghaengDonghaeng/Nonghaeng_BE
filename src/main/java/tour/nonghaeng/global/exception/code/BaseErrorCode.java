4cb5516fc47654d269372b15b9d839dac67a469a
Changmin Han <hcmhcs0@ajou.ac.kr>
2024-03-06 12:49:47 +0900

    feat/exception: BaseErrorCode, GlobalErrorCode 추가

--- src/main/java/tour/nonghaeng/global/exception/code/BaseErrorCode.java
+++ src/main/java/tour/nonghaeng/global/exception/code/BaseErrorCode.java
@@ -1,2 +1,7 @@
-package tour.nonghaeng.global.exception.code;public interface BaseErrorCode {
+package tour.nonghaeng.global.exception.code;
+
+import tour.nonghaeng.global.exception.dto.ErrorReason;
+
+public interface BaseErrorCode {
+    public ErrorReason getErrorReason();
 }
--- src/main/java/tour/nonghaeng/global/exception/code/GlobalErrorCode.java
+++ src/main/java/tour/nonghaeng/global/exception/code/GlobalErrorCode.java
@@ -1,2 +1,26 @@
-package tour.nonghaeng.global.exception.code;public enum GlobalErrorCode {
+package tour.nonghaeng.global.exception.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import tour.nonghaeng.global.exception.dto.ErrorReason;
+
+@AllArgsConstructor
+@Getter
+public enum GlobalErrorCode implements BaseErrorCode{
+    ALL_NOT_FOUND(HttpStatus.NOT_FOUND,"EXAM_404_1","전체 오류")
+    ;
+
+    private HttpStatus status;
+    private String code;
+    private String reason;
+
+    @Override
+    public ErrorReason getErrorReason() {
+        return ErrorReason.builder()
+                .status(this.status)
+                .code(this.code)
+                .reason(this.reason)
+                .build();
+    }
 }
