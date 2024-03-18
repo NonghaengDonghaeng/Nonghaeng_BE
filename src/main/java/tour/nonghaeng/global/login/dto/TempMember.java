f5a9c5b749896613d6df0aa0bcfac846cd74e7a1
Changmin Han <hcmhcs0@ajou.ac.kr>
2024-03-18 14:00:39 +0900

    fix/member: tempMember 파일타입 변경
    - record파일에서 class파일로 변경

--- src/main/java/tour/nonghaeng/global/login/dto/TempMember.java
+++ src/main/java/tour/nonghaeng/global/login/dto/TempMember.java
@@ -1,9 +1,22 @@
 package tour.nonghaeng.global.login.dto;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import tour.nonghaeng.domain.etc.role.Role;
 
-public record TempMember(
-        String username,
-        String password,
-        Role role) {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class TempMember {
+    private String username;
+    private String password;
+    private Role role;
+
+    @Builder
+    public TempMember(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
 }
