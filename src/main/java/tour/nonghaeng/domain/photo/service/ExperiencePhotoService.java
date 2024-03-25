712ccda460e85c49d9398201a9446a8d98b97e07
Changmin Han <hcmhcs0@ajou.ac.kr>
2024-03-25 16:27:49 +0900

    feat/photo: ExperincePhotoService에 여러 로직 작성
    - upload(): S3에 이미지를 업로드하고 반환받은 url을 ExperiencePhoto DB에 저장
    - delete(): S3에 올라간 이미지를 삭제하고 ExperiencePhoto DB 내에서도 삭제

--- src/main/java/tour/nonghaeng/domain/photo/service/ExperiencePhotoService.java
+++ src/main/java/tour/nonghaeng/domain/photo/service/ExperiencePhotoService.java
@@ -4,7 +4,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import tour.nonghaeng.domain.etc.photo.PhotoType;
+import tour.nonghaeng.domain.experience.entity.Experience;
+import tour.nonghaeng.domain.experience.service.ExperienceService;
+import tour.nonghaeng.domain.member.entity.Seller;
+import tour.nonghaeng.domain.photo.entity.ExperiencePhoto;
 import tour.nonghaeng.domain.photo.repo.ExperiencePhotoRepository;
+import tour.nonghaeng.domain.s3.AmazonS3Service;
+import tour.nonghaeng.global.exception.PhotoException;
+import tour.nonghaeng.global.validation.photo.ExperiencePhotoValidator;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +21,56 @@ import tour.nonghaeng.domain.photo.repo.ExperiencePhotoRepository;
 @Slf4j
 public class ExperiencePhotoService {
 
+    private static final PhotoType PHOTO_TYPE = PhotoType.EXPERIENCE;
+
     private final ExperiencePhotoRepository experiencePhotoRepository;
+
+    private final ExperienceService experienceService;
+    private final AmazonS3Service amazonS3Service;
+
+    private final ExperiencePhotoValidator experiencePhotoValidator;
+
+
+    public Long upload(Seller seller, Long experienceId, MultipartFile imageFile) {
+
+        Experience experience = experienceService.findById(experienceId);
+
+        String imgUrl = amazonS3Service.uploadImage(PHOTO_TYPE, imageFile);
+
+        return createExperiencePhoto(experience, imgUrl).getId();
+
+    }
+
+    public void delete(Seller seller, Long experiencePhotoId) {
+
+        experiencePhotoValidator.ownerValidate(seller, experiencePhotoId);
+
+        amazonS3Service.deleteImage(PHOTO_TYPE, getUrlById(experiencePhotoId));
+
+        deleteExperiencePhoto(experiencePhotoId);
+    }
+
+    private ExperiencePhoto createExperiencePhoto(Experience experience, String imgUrl) {
+
+        return experiencePhotoRepository.save(ExperiencePhoto.builder()
+                .experience(experience)
+                .imgUrl(imgUrl)
+                .build());
+    }
+
+    private void deleteExperiencePhoto(Long experiencePhotoId) {
+
+        experiencePhotoRepository.delete(findById(experiencePhotoId));
+    }
+
+    private String getUrlById(Long experiencePhotoId) {
+
+        return findById(experiencePhotoId).getImgUrl();
+    }
+
+    public ExperiencePhoto findById(Long experiencePhotoId) {
+
+        return experiencePhotoRepository.findById(experiencePhotoId)
+                .orElseThrow(() -> PhotoException.EXCEPTION);
+    }
 }
