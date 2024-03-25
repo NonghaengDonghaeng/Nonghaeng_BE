9928f4d3c431f1066c2dd8e6fd44c64505e5e02e
Changmin Han <hcmhcs0@ajou.ac.kr>
2024-03-25 16:32:20 +0900

    feat/photo: ExperiencePhotoController에 업로드,삭제 API 함수 작
    - upload API 작성
    - delete API 작성

--- src/main/java/tour/nonghaeng/domain/photo/controller/ExperiencePhotoController.java
+++ src/main/java/tour/nonghaeng/domain/photo/controller/ExperiencePhotoController.java
@@ -2,8 +2,12 @@ package tour.nonghaeng.domain.photo.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import tour.nonghaeng.domain.member.entity.Seller;
 import tour.nonghaeng.domain.photo.service.ExperiencePhotoService;
 import tour.nonghaeng.global.auth.service.AuthService;
 
@@ -15,4 +19,27 @@ public class ExperiencePhotoController {
 
     private final ExperiencePhotoService experiencePhotoService;
     private final AuthService authService;
+
+    @PostMapping("/seller/upload/{experienceId}")
+    public ResponseEntity<String> upload(Authentication authentication,
+                                         @RequestPart("image") MultipartFile imageFile,
+                                         @PathVariable("experienceId") Long experienceId) {
+
+        Seller seller = authService.toSellerEntity(authentication);
+
+        Long uploadId = experiencePhotoService.upload(seller, experienceId, imageFile);
+
+        return new ResponseEntity<>("업로드 완료. id:" + String.valueOf(uploadId), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/seller/{experiencePhotoId}")
+    public ResponseEntity<String> delete(Authentication authentication,
+                                         @PathVariable("experiencePhotoId") Long experiencePhotoId) {
+
+        Seller seller = authService.toSellerEntity(authentication);
+
+        experiencePhotoService.delete(seller, experiencePhotoId);
+
+        return new ResponseEntity<>("삭제완료", HttpStatus.OK);
+    }
 }
