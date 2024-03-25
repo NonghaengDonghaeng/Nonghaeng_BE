c8bff604b387bbcb81897729ab04496009853015
Changmin Han <hcmhcs0@ajou.ac.kr>
2024-03-25 16:09:41 +0900

    feat/photo: RoomPhotoController에 업로드,삭제 API 함수 작성

--- src/main/java/tour/nonghaeng/domain/photo/controller/RoomPhotoController.java
+++ src/main/java/tour/nonghaeng/domain/photo/controller/RoomPhotoController.java
@@ -2,8 +2,12 @@ package tour.nonghaeng.domain.photo.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import tour.nonghaeng.domain.member.entity.Seller;
 import tour.nonghaeng.domain.photo.service.RoomPhotoService;
 import tour.nonghaeng.global.auth.service.AuthService;
 
@@ -15,4 +19,27 @@ public class RoomPhotoController {
 
     private final RoomPhotoService roomPhotoService;
     private final AuthService authService;
+
+    @PostMapping("/seller/upload/{roomId}")
+    public ResponseEntity<String> upload(Authentication authentication,
+                                         @RequestPart("image") MultipartFile imageFile,
+                                         @PathVariable("roomId") Long roomId) {
+
+        Seller seller = authService.toSellerEntity(authentication);
+
+        Long uploadId = roomPhotoService.upload(seller, roomId, imageFile);
+
+        return new ResponseEntity<>("업로드 완료. id:" + String.valueOf(uploadId), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/seller/{roomPhotoId}")
+    public ResponseEntity<String> delete(Authentication authentication,
+                                         @PathVariable("roomPhotoId") Long roomPhotoId) {
+
+        Seller seller = authService.toSellerEntity(authentication);
+
+        roomPhotoService.delete(seller, roomPhotoId);
+
+        return new ResponseEntity<>("삭제완료", HttpStatus.OK);
+    }
 }
