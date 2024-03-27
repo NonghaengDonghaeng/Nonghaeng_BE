544c524c93e5ced6bf856189eb0bfc628ef5292e
Changmin Han <hcmhcs0@ajou.ac.kr>
2024-03-27 15:45:27 +0900

    feat/photo: RoomPhotoRepository에 숙소대표사진 1개인지 확인함수, 대표사진의 갯수, 대표사진의 id찾는 함수, 숙소에 해당하는 사진리스트 보는 함수 작성

--- src/main/java/tour/nonghaeng/domain/photo/repo/RoomPhotoRepository.java
+++ src/main/java/tour/nonghaeng/domain/photo/repo/RoomPhotoRepository.java
@@ -1,11 +1,32 @@
 package tour.nonghaeng.domain.photo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import tour.nonghaeng.domain.photo.entity.Photo;
 import tour.nonghaeng.domain.photo.entity.RoomPhoto;
+import tour.nonghaeng.domain.room.entity.Room;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RoomPhotoRepository extends JpaRepository<RoomPhoto, Long> {
 
     boolean existsById(Long tourPhotoId);
+
+    @Query("select count(rp) from RoomPhoto rp where rp.room = :room and rp.representative=true")
+    Integer countRepresentative(@Param("room") Room room);
+
+    @Query("select count(rp) = 1 from RoomPhoto rp where rp.room = :room and rp.representative=true")
+    boolean hasExactlyOneRepresentativePhoto(@Param("room") Room room);
+
+    @Query("select rp.id from RoomPhoto rp where rp.room = :room and rp.representative = true")
+    Optional<Long> findRepresentativePhotoId(@Param("room") Room room);
+
+    @Query("select rp from RoomPhoto rp where rp.room = :room")
+    List<Photo> findAllByRoom(@Param("room") Room room);
+
+
 }
