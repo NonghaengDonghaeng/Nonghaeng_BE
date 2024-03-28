b30929e90be28e3d729f3afe0d3ef2f7e0590f7f
Changmin Han <hcmhcs0@ajou.ac.kr>
2024-03-28 14:35:02 +0900

    fix/reservation: 숙소예약,체험예약 비슷한 부분이 많기에 Reservation 상속관계로 변경, db는 join전략 매핑
    - entity: Reservation을 상속받도록 숙소예약,체험예약 엔티티 수정

--- src/main/java/tour/nonghaeng/domain/reservation/entity/ExperienceReservation.java
+++ src/main/java/tour/nonghaeng/domain/reservation/entity/ExperienceReservation.java
@@ -5,33 +5,20 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import tour.nonghaeng.domain.etc.BaseTimeEntity;
 import tour.nonghaeng.domain.etc.reservation.ReservationStateType;
 import tour.nonghaeng.domain.experience.entity.Experience;
 import tour.nonghaeng.domain.experience.entity.ExperienceRound;
-import tour.nonghaeng.domain.member.entity.Seller;
 import tour.nonghaeng.domain.member.entity.User;
+import tour.nonghaeng.domain.reservation.controller.Reservation;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "EXPERIENCE_RESERVATIONS")
+@DiscriminatorValue("experience_reservation")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class ExperienceReservation extends BaseTimeEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "experience_reservation_id")
-    private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    @ManyToOne
-    @JoinColumn(name = "seller_id")
-    private Seller seller;
+public class ExperienceReservation extends Reservation {
 
     @ManyToOne
     @JoinColumn(name = "experience_id")
@@ -41,56 +28,18 @@ public class ExperienceReservation extends BaseTimeEntity {
     @JoinColumn(name = "experience_round_id")
     private ExperienceRound experienceRound;
 
-    @Enumerated(EnumType.STRING)                //
-    private ReservationStateType stateType;
-
-    private int price;
-
     private LocalDate reservationDate;
 
     private int numOfParticipant;
 
-    private String reservationName;     //예약자명
-
-    private String number;
-
-    private String email;
 
     @Builder
     public ExperienceReservation(User user, ExperienceRound experienceRound, ReservationStateType stateType, int price, LocalDate reservationDate, int numOfParticipant, String reservationName, String number, String email) {
-        this.user = user;
-        this.seller = experienceRound.getExperience().getSeller();
+        super(user,experienceRound.getExperience().getSeller(),stateType,price,reservationName,number,email);
         this.experience = experienceRound.getExperience();
         this.experienceRound = experienceRound;
-        this.stateType = stateType;
-        this.price = price;
         this.reservationDate = reservationDate;
         this.numOfParticipant = numOfParticipant;
-        this.reservationName = reservationName;
-        this.number = number;
-        this.email = email;
+
     }
-
-    public void approveReservation() {
-        //여기서 if문을 제외해도 될 것 같음. 어차피 검증로직을 거치고 승인되니까 2연속으로 해야될지 고민중
-        if (this.stateType.equals(ReservationStateType.WAITING_RESERVATION)) {
-            this.stateType = ReservationStateType.CONFIRM_RESERVATION;
-        }
-    }
-
-    public void notApproveReservation() {
-
-        if (this.stateType.equals(ReservationStateType.WAITING_RESERVATION)) {
-            this.stateType = ReservationStateType.NOT_CONFIRM_RESERVATION;
-        }
-    }
-
-    public ExperienceReservation cancelReservation() {
-
-        this.stateType = ReservationStateType.CANCEL_RESERVATION;
-        return this;
-    }
-
-
-
 }
--- src/main/java/tour/nonghaeng/domain/reservation/entity/RoomReservation.java
+++ src/main/java/tour/nonghaeng/domain/reservation/entity/RoomReservation.java
@@ -5,10 +5,9 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import tour.nonghaeng.domain.etc.BaseTimeEntity;
 import tour.nonghaeng.domain.etc.reservation.ReservationStateType;
-import tour.nonghaeng.domain.member.entity.Seller;
 import tour.nonghaeng.domain.member.entity.User;
+import tour.nonghaeng.domain.reservation.controller.Reservation;
 import tour.nonghaeng.domain.room.entity.Room;
 
 import java.util.ArrayList;
@@ -18,20 +17,7 @@ import java.util.List;
 @Table(name = "ROOM_RESERVATIONS")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class RoomReservation extends BaseTimeEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "room_reservation_id")
-    private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    @ManyToOne
-    @JoinColumn(name = "seller_id")
-    private Seller seller;
+public class RoomReservation extends Reservation {
 
     @ManyToOne
     @JoinColumn(name = "room_id")
@@ -41,58 +27,21 @@ public class RoomReservation extends BaseTimeEntity {
             cascade = CascadeType.ALL, orphanRemoval = true)
     private List<RoomReservationDate> reservationDates = new ArrayList<>();
 
-
-    @Enumerated(EnumType.STRING)
-    private ReservationStateType stateType;
-
-    private int price;
-
     private int numOfRoom;
 
     private int numOfParticipant;
 
-    private String reservationName;
-
-    private String number;
-
-    private String email;
 
     @Builder
     private RoomReservation(User user, Room room, ReservationStateType stateType, int price, int numOfRoom, int numOfParticipant, String reservationName, String number, String email) {
-
-        this.user = user;
-        this.seller = room.getSeller();
+        super(user,room.getSeller(),stateType,price,reservationName,number,email);
         this.room = room;
-        this.stateType = stateType;
-        this.price = price;
         this.numOfRoom = numOfRoom;
         this.numOfParticipant = numOfParticipant;
-        this.reservationName = reservationName;
-        this.number = number;
-        this.email = email;
     }
 
     public void addRoomReservationDate(RoomReservationDate roomReservationDate) {
         this.reservationDates.add(roomReservationDate);
     }
 
-    public void approveReservation() {
-
-        if (this.stateType.equals(ReservationStateType.WAITING_RESERVATION)) {
-            this.stateType = ReservationStateType.CONFIRM_RESERVATION;
-        }
-    }
-
-    public void notApproveReservation() {
-
-        if (this.stateType.equals(ReservationStateType.WAITING_RESERVATION)) {
-            this.stateType = ReservationStateType.NOT_CONFIRM_RESERVATION;
-        }
-    }
-
-    public RoomReservation cancelReservation() {
-
-        this.stateType = ReservationStateType.CANCEL_RESERVATION;
-        return this;
-    }
 }
