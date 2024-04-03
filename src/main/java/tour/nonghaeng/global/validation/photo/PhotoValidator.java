056c32842071421af2c0c22827ee23dda3b3fd13
Changmin Han <hcmhcs0@ajou.ac.kr>
2024-04-03 16:03:50 +0900

    fix/photo: PhotoValidator에 삭제검증 로직 추가
    -validator: 삭제할 photoId가 대표사진인지 검증로직 작성, 공통의 PhotoValidator에서 체크할 예정

--- src/main/java/tour/nonghaeng/global/validation/photo/PhotoValidator.java
+++ src/main/java/tour/nonghaeng/global/validation/photo/PhotoValidator.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import tour.nonghaeng.domain.photo.entity.Photo;
+import tour.nonghaeng.domain.photo.repo.PhotoRepository;
 import tour.nonghaeng.global.exception.PhotoException;
 import tour.nonghaeng.global.exception.code.PhotoErrorCode;
 
@@ -14,10 +15,19 @@ import java.util.List;
 @Slf4j
 public class PhotoValidator {
 
+    private final PhotoRepository photoRepository;
+
     public void emptyPhotoListValidate(List<Photo> photoList) {
 
         if (photoList.isEmpty()) {
             throw new PhotoException(PhotoErrorCode.EMPTY_PHOTO_LIST_ERROR);
+        }
+    }
+
+    public void deletePhotoValidate(Long photoId) {
+
+        if (photoRepository.findRepresentById(photoId)) {
+            throw new PhotoException(PhotoErrorCode.CANT_DELETE_REPRESENTATIVE_PHOTO_ERROR);
         }
     }
 
