7d4f62ac696f93c8b0be87ef8af4e41b4d5d8a1b
Changmin Han <hcmhcs0@ajou.ac.kr>
2024-06-14 16:03:46 +0900

    chore/payment: 포트원 도입을 위한 Config 설정 및 의존관계 추가

--- src/main/java/tour/nonghaeng/global/config/SecurityConfig.java
+++ src/main/java/tour/nonghaeng/global/config/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequest -> authorizeRequest
                         .requestMatchers("/").permitAll()
+                        .requestMatchers("/static1/**").permitAll()
                         .requestMatchers("/test/exception").permitAll()
                         .requestMatchers("/test/user-role").hasRole(Role.USER.name())
                         .requestMatchers("/test/seller-role").hasRole(Role.SELLER.name())
